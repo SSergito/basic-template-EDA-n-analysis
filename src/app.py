8239b0799248068c2ab957eda511f103e8540243
Sergio Roque Hernández <sergioroquehernandez93@gmail.com>
2025-04-03 19:59:17 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
+import pandas as pd
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+# Me traigo los datos de la url proporcionada y los guardo localmente en mi proyecto
+# url = "https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv"
+# df = pd.read_csv(url)
+# df.to_csv("data/raw/data.csv", index=False)
+
+df = pd.read_csv("data/raw/data.csv")
+print(df.head())
